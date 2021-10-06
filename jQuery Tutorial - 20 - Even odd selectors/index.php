1c0aa7b99f60e5c3a34ba295afd89fe6bff19810
ArifulSikder <mdislamnoyon3@gmail.com>
2021-10-07 00:32:25 +0600

    21 out of 200

---
+++ jQuery Tutorial - 20 - Even odd selectors/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <td>row 1</td>
+        </tr>
+        <tr>
+            <td>row 1</td>
+        </tr>
+        <tr>
+            <td>row 1</td>
+        </tr>
+        <tr>
+            <td>row 1</td>
+        </tr>
+        <tr>
+            <td>row 1</td>
+        </tr>
+        <tr>
+            <td>row 1</td>
+        </tr>
+    </table>
+
+    <div class="para">
+        <p>para 1</p>
+        <p>para 2</p>
+        <p>para 3</p>
+        <p>para 4</p>
+    </div>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/selector.js"></script>
+</body>
+
+</html>
---
+++ jQuery Tutorial - 21 - Attribute selectors/js/selector.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $('table tr:even, .para p:odd').addClass('highlight');
+});
