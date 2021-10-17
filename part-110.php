dbf0284db76fa8569f59b43908fc130ee27706aa
md-sohan719 <81063728+md-sohan719@users.noreply.github.com>
2021-10-17 17:17:41 +0600

    update

---
+++ part-110.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jQuery</title>
+    <style></style>
+</head>
+
+<body>
+
+    Email: <input type="text" id="email"><span id="email_feedback"></span>
+
+    <script src="js/jquery.min.js"></script>
+    <script>
+        function validate_email(email) {
+            $.post('php/email.php', {
+                email: email
+            }, function(data) {
+                $('#email_feedback').text(data);
+            });
+        }
+        $('#email').focusin(function() {
+            if ($('#email').val() === '') {
+                $('#email_feedback').text('Go on, enter a valid email address....');
+            } else {
+                validate_email($('#email').val());
+            }
+        }).blur(function() {
+            $('#email_feedback').text('');
+        }).keyup(function() {
+            validate_email($('#email').val());
+        });
+    </script>
+
+</body>
+
+</html>
---
+++ part-111.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jQuery</title>
+    <style></style>
+</head>
+
+<body>
+
+    <h3>Setting</h3>
+
+    <p> Name: <input type="text" name="" id="name"></p>
+    <p>
+        <input type="button" id="save_button" value="Save">
+        <span id="save_status"></span>
+    </p>
+
+    <script src="js/jquery.min.js"></script>
+    <script>
+
+    </script>
+
+</body>
+
+</html>
