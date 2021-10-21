7218566b089eea15d610dae36a4eacef5c2a4ffd
ArifulSikder <67917301+ArifulSikder@users.noreply.github.com>
2021-10-21 15:42:35 +0600

    part 49 out of 200

--- jQuery Tutorial - 150 - Resizable/index.php
+++ jQuery Tutorial - 150 - Resizable/index.php
@@ -6,24 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="jquery-ui-1.9.2.custom.min.css">
+    <link rel="stylesheet" href="css/base/jquery-ui-1.9.2.custom.min.css">
     <title>document</title>
 </head>
 
 <body>
-    <ul id="name">
-        <li>Profile</li>
-        <li>Settings</li>
-        <li>Login</li>
-        <li>Register</li>
-    </ul>
-    <ul id="places">
-        <li>Rajbari</li>
-        <li>Foridpur</li>
-        <li>Dhaka</li>
-        <li>Rongpur</li>
-    </ul>
-    <div id="showUpdate"></div>
+   <div id="big_area">
+       <div id="resizeAria"></div>
+   </div>
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/jquery-ui.js"></script>
     <script type="text/javascript" src="js/ext.js"></script>
