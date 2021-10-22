4d7101f5d41f81995c1f419b952c455f98af429e
ArifulSikder <mdislamnoyon3@gmail.com>
2021-10-22 19:01:23 +0600

    part 152 out of 200

---
+++ jQuery Tutorial - 152 - Accordion/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/base/jquery-ui-1.9.2.custom.min.css">
+    <title>document</title>
+</head>
+
+<body>
+    <div id="content">
+        <h3><a href="#">About Me</a></h3>
+        <div>A title about me!</div>
+        <h3><a href="#">Portfolio</a></h3>
+        <div>This is my portfolio!</div>
+        <h3><a href="#">Contact Us</a></h3>
+        <div>My contact details!</div>
+    </div>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.js"></script>
+    <script type="text/javascript" src="js/ext.js"></script>
+</body>
+
+</html>
