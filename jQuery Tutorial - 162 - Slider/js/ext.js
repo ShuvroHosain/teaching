6f1f3a1d9cd22d250153acbce7123c21bfe432bf
ArifulSikder <67917301+ArifulSikder@users.noreply.github.com>
2021-11-06 15:45:46 +0600

    part 163 out of 200

--- jQuery Tutorial - 162 - Slider/js/ext.js
+++ jQuery Tutorial - 162 - Slider/js/ext.js
@@ -2,12 +2,18 @@ $(document).ready(function () {
   min_slider = 1;
   max_siider = 400;
   $("#slider").slider({
-    step: 4,
     min: min_slider,
     max: max_siider,
+    // step: 5,
+    range: true,
+    values: [20, 40],
+    orientation: 'vertical',
     slide: function(event, ui) {
-      $("#slider_value").html("value" + ui.value);
+      $("#slider_value").html("value One: " + ui.values[0] + "value two: "+ ui.values[1]);
     },
+    stop: function (e, ui) {
+      alert(ui.value);
+    }
   });
 });
 
---
+++ z-exam/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/base/jquery-ui-1.9.2.custom.min.css">
+    <title>document</title>
+</head>
+
+<body>
+    
+
+  
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.js"></script>
+    <script type="text/javascript" src="development-bundle/external/jquery.cookie.js"></script>
+    <script type="text/javascript" src="js/ext.js"></script>
+</body>
+
+</html>
